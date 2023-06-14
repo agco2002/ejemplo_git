492c279fafdf8662db9d494b824d40181b16438e
Andres <agco7331@gmail.com>
2023-06-14 13:56:10 -0500

    escribiendo comandos en git

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estudiando git</title>
+</head>
+<body>
+  <h1>Concepto de git</h1>
+  <p>Git is the open source distributed version control system that facilitates GitHub activities on
+your laptop or desktop. This cheat sheet summarizes commonly used Git command line
+instructions for quick reference.</p>
+
+<h2>Comandos principales de git</h2>
+
+<ul>
+  <li><b> git init:</b> Me permite iniciar un proyecto en git hola</li>
+  <li><b>git config --global user.name Andres Calderon </b>Nos permite gurdar el nombre en el repositorio local</li>
+  <li><b>git config --global --unset-all user.name</b>Elimina las configuraciones guardadas en el repositorio </li>
+  <li>clean: Nos permite limpiar la pantalla del terminal</li>
+  <li>cd "Nombre del directorio": Ingresa un directorio en una terminal</li>
+  <li><b>cd ..</b> Nos permite salir del directorio en la terminal</li>
+  <li><b>pwd</b> Nos permite mostrar la ruta del directorio actual en la terminal</li>
+  <li><b>ls</b>Muestra la lista de contenidos de un directorio</li>
+  <li><b>git log</b>Muestra el historial de cambios en el repositorio</li>
+</ul>
+</body>
+</html>
